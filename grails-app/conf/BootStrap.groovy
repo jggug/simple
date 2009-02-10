b678cf84f777d8fac9f94be8def8a52b70495e54
ponytail.the.s <ponytail.the.s@d49d29ce-d3ce-11dd-a412-a1792178251d>
2009-02-10 08:45:52 +0000

    DBが空だった場合に初期ユーザを作る
    git-svn-id: https://gcrnagoya.googlecode.com/svn/trunk/simple@31 d49d29ce-d3ce-11dd-a412-a1792178251d

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,28 @@
-class BootStrap {
+import org.grails.plugins.springsecurity.service.AuthenticateService
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+class BootStrap {
+	AuthenticateService authenticateService
+
+  def init = { servletContext ->
+    def adminName="admin"
+    def adminRealName="管理者"
+    def adminEmail="admin@grails.jp"
+    def password="grailsRock"
+    
+    // ユーザがなかった場合にデフォロールとデフォ管理者を作ります
+    if(Person.list().size()==0) {
+      def r_user=new Authority(authority:"ROLE_USER",description:"").save(flush:true)
+      def r_admin=new Authority(authority:"ROLE_ADMIN",description:"").save(flush:true)
+      
+  		def pass = authenticateService.passwordEncoder(password)
+      def admin=new Person(username:adminName,userRealName:adminRealName,enabled:true,email:adminEmail,emailShow:true,passwd:pass).save(flush:true)
+      r_user.addToPeople(admin)
+      r_admin.addToPeople(admin)
+      r_user.save(flush:true)
+      r_admin.save(flush:true)
+      println "create default role & user"
+    }
+  }
+  def destroy = {
+  }
 } 
